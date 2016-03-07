de25ac741580b25fcc7c1b2031bcfe0a0b8585e2
bhagwati1586 <shonena4u@gmail.com>
2016-03-07 09:35:07 +0530

    First

--- src/about/components/about.ts
+++ src/about/components/about.ts
@@ -12,6 +12,12 @@ import {NameList} from '../../shared/services/name_list';
 })
 export class AboutCmp {
   newName: string;
+  empName : string;
+  empAge : string;
+  error : boolean;
+  edit : boolean;
+  index : number;
+  errorMsg : string;
   constructor(public list: NameList) {}
  /*
  * @param newname  any text as input.
@@ -19,16 +25,60 @@ export class AboutCmp {
  */
 
   addName(): boolean {
-    this.list.add(this.newName);
-    this.newName = '';
+    if(this.newName) {
+      this.list.add(this.newName);
+      this.newName = '';
+      this.error = false;
+    }else { this.errorMsg = 'Enter Scientist Name'; this.error = true;}
     return false;
   }
 
+
   deleteName(i): boolean {
+    this.error = false;
+    this.errorMsg = '';
     this.list.delete(i);
     this.newName = '';
     return false;
   }
 
+  addEmp(): boolean {
+    if(this.empName) {
+      this.list.addEmp(this.empName, this.empAge);
+      this.empName = '';
+      this.empAge='';
+      this.error = false;
+    }else {
+      this.errorMsg = 'Enter Scientist Name';
+      this.error = true;
+    }
+    return false;
+  }
 
+  deleteEmp(i): boolean {
+    this.error = false;
+    this.errorMsg = '';
+    this.list.deleteEmp(i);
+    this.empName = '';
+    return false;
+  }
+
+  editEmp(i) : boolean {
+    var n = this.list.getEmp();
+    this.empName = n[i].firstName;
+    this.empAge = n[i].age;
+    this.edit = true;
+    this.index = i;
+    return false;
+
+  }
+  updateEmp(i):boolean {
+    this.list.updateEmp(i,this.empName,this.empAge);
+    this.empName = '';
+    this.empAge='';
+    this.error = true;
+    this.edit = false;
+    this.errorMsg ='Record Updated';
+    return false;
+  }
 }
--- src/shared/services/name_list.ts
+++ src/shared/services/name_list.ts
@@ -1,6 +1,12 @@
 export class NameList {
   names = ['Bhagwati', 'Jatin', 'Apoorv', 'Rehan'];
 
+  emp = {'employees':[
+    {'firstName':'Bhagwati', 'age':'30'},
+    {'firstName':'Jatin', 'age':'31'},
+    {'firstName':'Apoorv', 'age':'45'}
+  ]};
+
   get(): string[] {
     return this.names;
   }
@@ -10,4 +16,21 @@ export class NameList {
   delete(value: number): void {
     this.names.splice(value,1);
   }
+
+  addEmp(firstName: string, age : string): void {
+    this.emp.employees.push({'firstName':firstName, 'age':age});
+  }
+  deleteEmp(value: number): void {
+    this.emp.employees.splice(value,1);
+  }
+  updateEmp(value: number, firstName: string, age : string): void {
+
+    this.emp.employees[value].firstName = firstName;
+    this.emp.employees[value].age = age;
+  }
+
+  getEmp() {
+    return this.emp.employees;
+  }
+
 }
